51b97c6152d7b5333f7e4c7bde03cfb1042e08cb
Lawrence <liang-lawrence.liu@schneider-electric.com>
2016-10-31 17:07:35 +0800

    change server

--- server.js
+++ server.js
@@ -134,3 +134,7 @@ var actions = {
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
