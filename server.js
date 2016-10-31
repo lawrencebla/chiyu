e6afacfd40117f8b824358a67048e58cc8334f62
Lawrence <liang-lawrence.liu@schneider-electric.com>
2016-10-31 17:52:35 +0800

    change server

--- server.js
+++ server.js
@@ -1,28 +1,140 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var path = require("path");
+var fs = require("fs");
+var express = require('express');
+var UUID = require('node-uuid');
+var app = express();
+var WebSocketServer = require('ws').Server;
+var server = require('http').createServer(/*{
+  key: fs.readFileSync(path.join(__dirname, './keys', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, './keys', 'cert.pem'))	
+}, */app);
 
-app.use(express.static(__dirname + "/"))
+var clientSockets = [];
+var hostSocket = null;
 
-var server = http.createServer(app)
-server.listen(port)
+function saveSend(sk, message) {
+	if(sk && sk.OPEN) {
+		sk.send(message);
+	} else {
+		console.log('current socket is closed');
+	}
+}
 
-console.log("http server listening on %d", port)
+function createClients(clientSockets) {
+	clientSockets.forEach(function(csk) {
+		console.log('createPeer');
+		csk.send(JSON.stringify({
+			actionType: 'createPeer',
+			data: csk.id
+		}));
+	});
+	hostSocket.send(JSON.stringify({
+		actionType: 'createStream',
+		data: clientSockets.map( function(client) {
+				return client.id;
+			} )
+	}));
+}
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+function findClientById(socketId) {
+	return clientSockets.filter( function(cs) {return cs.id === socketId} )[0];
+}
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+var actions = {
+	uploadOffer: function(data) {
+		console.log('downloadOffer');
+		saveSend(findClientById(data.socketId), JSON.stringify({
+			actionType: 'downloadOffer',
+			data: data
+		}));
+	},
+	uploadAnswer: function(data) {
+		console.log('downloadAnswer');
+		saveSend(hostSocket, JSON.stringify({
+			actionType: 'downloadAnswer',
+			data: data
+		}));
+	},
+	sendIceCandidateToClient: function(data) {
+		console.log('sendIceCandidateToClient');
+		saveSend(findClientById(data.socketId), JSON.stringify({
+			actionType: 'downloadIceCandidate',
+			data: data
+		}));
+	},
+	sendIceCandidateToHost: function(data) {
+		console.log('sendIceCandidateToHost');
+		saveSend(hostSocket, JSON.stringify({
+			actionType: 'downloadIceCandidate',
+			data: data,
+		}));
+	}
+}
 
-  console.log("websocket connection open")
+var ws = new WebSocketServer({
+	server: app
+});
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+ws.on('connection', function(sk) {
+	if( sk.upgradeReq.url !== '/server.html' ) {
+		console.log('client');
+		sk.id = UUID.v4();
+		clientSockets.push(sk);
+		if( hostSocket ) {
+			console.log('addClient');
+			createClients([sk]);
+		}
+		sk.on('close', function() {
+			if(hostSocket) {
+				console.log('removeClient');
+				hostSocket.send(JSON.stringify({
+					actionType: 'removeClient',
+					data: sk.id
+				}));
+				var clientLength = clientSockets.length;
+				if(clientLength > 0) {
+					for(var i = 0; i < clientLength ; i++) {
+						if( clientSockets[i].id === sk.id ) {
+							break;
+						}
+					}
+					if( i < clientLength ) {
+						clientSockets.splice(i, 1);
+					}
+				}				
+			}
+		});
+	} else if( !hostSocket && sk.upgradeReq.url === '/server.html' ) {
+		console.log('host');
+		hostSocket = sk;
+		createClients(clientSockets);
+		hostSocket.on('close', function() {
+			console.log('removeHost');
+			clientSockets.forEach(function(csk) {
+				saveSend(csk, JSON.stringify({
+					actionType: 'removeHost',
+				}));
+			});
+			hostSocket = null;
+		});
+	} else {
+		console.log('only has one host');
+		sk.close();		
+	}
+
+	sk.on('message', function(data) {
+		var json = JSON.parse(data);
+		console.log(json.actionType);
+		actions[json.actionType](json.data);
+	});
+
+});
+// console.error(process.env.PORT);
+server.listen(process.env.PORT || 5000);
+// app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
